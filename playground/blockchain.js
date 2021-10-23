667bc86407625c6430d7775aa7c02d7cc98f0f58
derektruong <minhduc0589@gmail.com>
2021-10-23 11:27:38 +0700

    fix cors

---
+++ playground/blockchain.js
@@ -0,0 +1,24 @@
+// class Blockchain {
+// 	constructor() {
+// 		// Create
+// 		this.chain = [];
+// 		// Transaction
+// 		this.curr_transaction = [];
+
+// 	}
+
+// 	addNewBox(prevHash) {
+// 		let block = {
+// 			index: this.chain.length + 1,
+// 			timestamp: Date.now(),
+// 			transaction: this.curr_transaction,
+// 			hash: null,
+// 			prevHash: prevHash,
+
+// 		};
+
+// 		// Add to chain
+// 		this.chain.push(block);
+
+// 	}
+// }
--- src/app.js
+++ src/app.js
@@ -4,14 +4,27 @@ const env = require("./config/env");
 const app = express();
 const PORT = env.PORT;
 
-app.get("/", (req, res) => {
-	res.send("<a href=\"/auth/google\">Authentication with Google</a>");
-});
-
-app.get("/protected", (req, res) => {
+app.all("/", function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+}, (req, res) => {
 	res.send("Hello");
 });
 
+// app.get("/", function (req, res, next) {
+//     // Handle the get for this route
+// });
+// app.post("/", function (req, res, next) {
+//     // Handle the post for this route
+// });
+
+
+
+app.get("/protected", (req, res) => {
+    res.send("Hello");
+});
+
 app.listen(PORT, () => {
-	console.log("Listening on port " + PORT);
+    console.log("Listening on port " + PORT);
 });
